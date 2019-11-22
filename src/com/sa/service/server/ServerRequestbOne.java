e505a27a0acad0507d69e55d63dba3bea4abd616
liyahui <15738773643@163.com>
2019-11-22 10:04:12 +0800

    updtae

--- src/com/sa/service/server/ServerRequestbOne.java
+++ src/com/sa/service/server/ServerRequestbOne.java
@@ -20,23 +20,30 @@ import com.sa.base.ServerManager;
 import com.sa.net.Packet;
 import com.sa.net.PacketType;
 
-public class ServerRequestbRoom extends Packet {
-	public ServerRequestbRoom(){}
+public class ServerRequestbOne extends Packet {
+	public ServerRequestbOne(){}
 	
 	@Override
 	public PacketType getPacketType() {
-		return PacketType.ServerRequestbRoom;
+		return PacketType.ServerRequestbOne;
 	}
 
 	@Override
 	public void execPacket() {
-		System.out.println("SEND ServerRequestbRoom " + this.getFromUserId() + "[" + this.getOption(1) + "]");
+		System.out.println("SEND ServerRequestbOne " + this.getFromUserId() + "[" + this.getOption(1) + "]");
 		this.setFromUserId("147080");
 		this.setRoomId("22421");
-		this.setTransactionId(15725);
+		this.setTransactionId(15724);
 		this.setStatus(0);
+		this.setToUserId("147080");
+		//this.setRemoteIp("192.168.21.115");
 		TreeMap<Integer,Object> treeMap = new TreeMap<>();
-		treeMap.put(1, "{'command':'open','content':'我是一班22421第一147080，我在ServerRequestbRoom','domain':'i-clicker','domain_id':37}");
+		treeMap.put(1, "{'command':'open','content':'我是一班22421第一147080，我给自己发了ServerRequestbOne','domain':'i-clicker','domain_id':37}");
+		//treeMap.put(2, "1");
+		//treeMap.put(3, "北京小低磨课2");
+		//treeMap.put(4, "");
+		//treeMap.put(5, "10366");
+		//treeMap.put(6, "E1Jt0lKKVbDsdh16XdhbFXKA18pk0f4m9kmipHaWVHs=");
 		this.setOptions(treeMap);
 		ServerManager.INSTANCE.sendServerRequest(this);
 	}
